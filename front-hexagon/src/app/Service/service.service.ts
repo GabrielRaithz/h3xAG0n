31fc12021512dd1f2d7019c4204e291ca673d6e9
Gabriel Raithz <gabrielraithz@icloud.com>
2019-08-26 15:16:15 -0300

    ajust port

--- front-hexagon/src/app/Service/service.service.ts
+++ front-hexagon/src/app/Service/service.service.ts
@@ -11,7 +11,7 @@ export class ServiceService {
   constructor(private http: HttpClient) { }
 
   UrlVehicle = 'http://ec2-18-231-109-21.sa-east-1.compute.amazonaws.com:8080/vehicle/';
-  UrlVehicleType = 'http://ec2-18-231-109-21.sa-east-1.compute.amazonaws.com/vehicle_type/';
+  UrlVehicleType = 'http://ec2-18-231-109-21.sa-east-1.compute.amazonaws.com:8080/vehicle_type/';
 
   getVehicles() {
     return this.http.get<Vehicle[]>(this.UrlVehicle);
