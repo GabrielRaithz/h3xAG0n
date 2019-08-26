0133b93420280a4dbacec089efae06f0ddcabd79
Gabriel Raithz <gabrielraithz@icloud.com>
2019-08-26 13:20:18 -0300

    sad

--- front-hexagon/vue.config.js
+++ front-hexagon/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/api': {
-          target: 'http://ec2-54-207-50-33.sa-east-1.compute.amazonaws.com:8080',
+          target: 'http://localhost:8080',
           pathRewrite: {'^/api' : ''}
         }
       }
