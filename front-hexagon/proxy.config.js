ba091ddc9e7f84c73aacaaf1e873e9ff73e866ad
Gabriel Raithz <gabrielraithz@icloud.com>
2019-08-26 14:58:39 -0300

    ajust rote

--- front-hexagon/proxy.config.js
+++
@@ -1,8 +0,0 @@
-const proxy = [
-    {
-      context: '/api',
-      target: 'http://ec2-54-207-50-33.sa-east-1.compute.amazonaws.com:8080',
-      pathRewrite: {'^/api' : ''}
-    }
-  ];
-module.exports = proxy;
